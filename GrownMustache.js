ab2f7cd64cc4974ba1699954b01fa0e20267d26b
SUKOHI <capilano.sukohi@gmail.com>
2020-01-21 02:52:42 +0900

    First commit

---
+++ GrownMustache.js
@@ -0,0 +1,151 @@
+const mustache = require('mustache');
+const fs = require('fs');
+const path = require('path');
+
+class GrownMustache {
+
+  constructor(options = {}) {
+
+    this.dir = (options.dir !== undefined) ? options.dir : '';
+    this.extension = (options.extension !== undefined) ? options.extension : 'mst';
+
+  }
+
+  render(path, params = []) {
+
+    const rawContent = this.getFileContent(path);
+    const lines = rawContent.split("\n");
+    let extensionPath = '';
+    let sectionKey = '';
+    let sectionLines = [];
+    let sections = {};
+
+    for(let rawLine of lines) {
+
+      const line = rawLine.toLowerCase().replace(/[\s"']/g, '');
+      const extendsMatches = line.match(/^@extends\(([a-z0-9\/]+)\)/);
+
+      if(extendsMatches) {
+
+        extensionPath = extendsMatches[1];
+        continue;
+
+      }
+
+      const sectionMatches = line.match(/^@section\(([a-z0-9]+)\)/);
+
+      if(sectionMatches) {
+
+        sectionKey = sectionMatches[1];
+        continue;
+
+      }
+
+      if(sectionKey !== '') {
+
+        if(line === '@stop') {
+
+          sections[sectionKey] = sectionLines.join("\n");
+          sectionKey = '';
+          sectionLines = [];
+          continue;
+
+        }
+
+        sectionLines.push(rawLine);
+
+      }
+
+    }
+
+    if(extensionPath !== '') {
+
+      const extensionContent = this.getFileContent(extensionPath);
+      const lines = extensionContent.split("\n");
+
+      for(let i in lines) {
+
+        let line = lines[i];
+        const matches = line.match(/@yield\(["']?([a-z0-9]+)["']?\)/);
+
+        if(matches) {
+
+          const sectionKey = matches[1];
+          const target = matches[0];
+          const replacement = (sections[sectionKey] !== undefined) ? sections[sectionKey] : '';
+          line = line.replace(target, replacement);
+
+        }
+
+        lines[i] = line;
+
+      }
+
+      const content = lines.join("\n");
+      return mustache.render(content, params);
+
+    }
+
+    return mustache.render(rawContent, params);
+
+  }
+
+  // Getter
+
+  getFilePath(filePath) {
+
+    const fullDir = path.resolve(this.dir);
+
+    if(filePath.startsWith(fullDir) && filePath.endsWith(this.extension)) {
+
+      return filePath;
+
+    }
+
+    return this.dir +'/'+ filePath +'.'+ this.extension;
+
+  }
+
+  getFileContent(path) {
+
+    const filePath = this.getFilePath(path);
+    return fs.readFileSync(filePath, 'utf8');
+
+  }
+
+  // Static
+  static express(path, options, callback) {
+
+    try {
+
+      const settings = options.settings;
+      const gm = new GrownMustache({
+        dir: settings.views,
+        extension: settings['view engine']
+      });
+      let params = {};
+
+      for(let key in options) {
+
+        if(!['settings', '_locals', 'cache'].includes(key)) {
+
+          params[key] = options[key];
+
+        }
+
+      }
+
+      const content = gm.render(path, params);
+      return callback(null, content);
+
+    } catch(error) {
+
+      return callback(error);
+
+    }
+
+  }
+
+}
+
+module.exports = GrownMustache;
---
+++ README.md
@@ -0,0 +1,99 @@
+# GrawnMustache.js
+
+A template package extended [mustache.js](https://github.com/janl/mustache.js) that provides "Template Extension".
+
+# Installation
+
+Run the following command.
+
+    npm i grown-mustache
+
+# Preparation
+
+Load the package as follows.
+
+    const GrownMustache = require('GrownMustache');
+
+# Usage
+
+## Basic usage
+
+The basic usage is the same with `mustache.js`.
+
+    const gm = new GrownMustache({
+        dir: './views',   // Optional
+        extension: 'mst'  // Optional
+    });
+
+    gm.render('index', {
+        message: 'Message'
+    });
+
+***Note:***  
+`index` means `./views/index.mst` in this case.  
+Or you also can use absolute path like `/home/your/path/to/template/index.mst` instead.
+
+## Extension
+
+This feature is inspired by [Blade](https://laravel.com/docs/6.x/blade).  
+As such, the usage is very similar.
+
+First, create a layout file called `layouts/app.mst` in this case.
+
+    <!DOCTYPE html>
+    <html lang="ja">
+      <head>
+        @yield('title')
+      </head>
+      <body>
+        <div id="app">
+          @yield('content')
+        </div>
+      </body>
+    </html>
+
+Secondally, create a main file called `index.mst`.
+
+    @extends('layouts/app')
+
+    @section('title')
+
+      <title>{{ title }}</title>
+
+    @stop
+
+    @section('content')
+
+      <h1>YOUR HEADING</h1>
+      <div id="app">
+          {{ message }}
+      </div>
+
+    @stop
+
+***Note:***  
+Of course, you can use "mustache" symbolizing `{{ *** }}` in both of the files.
+
+Finally, call `render()` as follows.
+
+    gm.render('index', {
+        title: 'Test title',
+        message: 'Test message'
+    });
+
+# Use in Express app
+
+`GrownMustache` has a callback function for Express.  
+Use it as follows.
+
+    const GrownMustache = require('GrownMustache');
+
+    // Template
+    app.engine('mst', GrownMustache.express);
+    app.set('views', './views');
+    app.set('view engine', 'mst');
+
+# License
+
+This package is licensed under the MIT License.  
+Copyright 2020 Sukohi Kuhoh
